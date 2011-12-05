16535116d5fb25c141f6117d29ed6155af407a64
root <root@debian>
2011-12-05 16:16:49 -0500

    fixed routing part

--- index.php
+++ index.php
@@ -2,24 +2,26 @@
 	include 'routes.php';
 	
 	function _get_path() {
-		$uri = $_SERVER['REQUEST_URI'];
-		$url = explode('?', $uri)[0];
+		$uri = explode('?', $_SERVER['REQUEST_URI']);
+		$url = $uri[0];
 		if (substr($uri, 0, 1) == '/') {
-			$uri = substr($uri, 1);
+			$url = substr($url, 1);
 		}
 		if (substr($uri, -1) == '/') {
-			$uri = substr($uri, 0, strlen($uri) - 2);
+			$url = substr($url, 0, strlen($url) - 2);
 		}
 
-		return $uri;
+		return $url;
 	}
 	
-	global $content = getcwd();
+    $content = getcwd();
 
-	if (array_key_exists(_get_path()), $_routes)) {
+	if (array_key_exists(_get_path(), $_routes)) {
 		
 		include $_routes[_get_path()];
+        include 'index.phtml';
 	} else {
-		$content = '/404error.phtml';
-	}	
+		$content .= '/404error.phtml';
+        include 'index.phtml';
+	}
 ?>
