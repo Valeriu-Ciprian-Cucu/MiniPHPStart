682544bc4e34cce23dcfe7699a65d3d2aa0c2b3d
Valeriu C. Cucu <vallerry_incense@yahoo.com>
2011-12-11 00:46:15 +0200

    fixed path inclusion

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 	define('ROOT', '/site/');
 
 	function _get_path() {
-		return '/' . substr($_SERVER['SCRIPT_FILENAME'], strlen(ROOT));
+		$url = explode('?', $_SERVER['REQUEST_URI']);
+		return substr($url[0], strlen(ROOT));
 	}
 	
-    global $_content;
-    $_content = getcwd();
+	global $_content;
 
 	if (array_key_exists(_get_path(), $_routes)) {	
 		include $_routes[_get_path()];
         include 'index.phtml';
 	} else {
-		$_content .= '/404error.phtml';
+		$_content = '404error.phtml';
         include 'index.phtml';
 	}
 ?>
