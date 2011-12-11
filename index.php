81ba72b6c0767408acda7d96a30de9ef5982a48a
Valeriu C. Cucu <valeriu.ciprian.cucu@gmail.com>
2011-12-11 19:36:06 +0200

    Added more documentation

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
+	#including file with routes
 	include 'routes.php';
 
+	#defining the ROOT constant for cases where the site doesn't go under the root folder but a subfolder
 	define('ROOT', '/site/');
 	
+	#global variable $_content is used in index.phtml to load the specific template code for the given action
 	global $_content;
 
+	#function for determining the route accessed
 	function _get_path() {
 		$url = explode('?', $_SERVER['REQUEST_URI']);
 		return substr($url[0], strlen(ROOT));
 	}
 
+	#function for loading the appropriate action and template code
 	function _include_files($path) {
-		global $_routes;
+		global $_routes, $_content;
 		include 'php/' . $_routes[$path]['action'] . '.php';
 
 		if ($_routes[$path]['template'] != false) {
@@ -20,6 +25,9 @@
 		}
 	}
 
+	/*	condition for loading the view code for the site root, code for matched route, or a
+	 *	custom error page for unmatched routes
+	 */
 	if (strlen(_get_path()) == 0) {
 		$_content = 'default.phtml';
 		include 'index.phtml';	
--- routes.php
+++ routes.php
@@ -1,4 +1,10 @@
 <?php
+	/*
+	 *	The $_routes variable stores all known routes ass associative arrays with 2 values (action and template).
+	 *	Action files are located in the <php> folder and all have the <.php> extension.
+	 *	Template files are stored in the <phtml> folder and all have the <.phtml> extension
+	 *	For routes where there is no view code to be generated (such as json/xml rpc webservices) set the template variable to <false>
+	 */
 	global $_routes;
 	$_routes = array(
 		'main' => array('action' => 'main', 'template' => 'main')	
